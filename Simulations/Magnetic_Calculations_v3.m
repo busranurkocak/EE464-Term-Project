9ec73776dc9524e3facca105f0a684d546b94c6e
DESKTOP-HUBL1OU\Asus <busranur.kocak26@gmail.com>
2021-04-26 08:08:21 +0300

    Updates on some values have been done

--- Simulations/Magnetic_Calculations_v3.m
+++ Simulations/Magnetic_Calculations_v3.m
@@ -1,15 +1,32 @@
 f = 100e3;
 D_max = 0.5;
-D_w = 0.1; %Dwell time duty ratio ?????????????????????????
+D_w = 0.1; %Dwell time duty ratio
 V_in_min = 220; %V
 V_out = 12;
 V_d = 1;
 n = 0.9; % efficiency
-B_max = 0.5; %change according to the material you have chosen
-Ku = 0.29;
-Ap = 0.59;
+Ku = 0.29; % Window Utilization
 Aw_26 = 0.00128; %Bare area of AWG26
 Wa = 2*8.6*(21.9-9.4)*(10^(-2));
+alfa = 3; %Regulation
+
+
+%Material specifications considering 3C90 as selected material type
+B_max = 0.25;
+mu_m = 2300;
+MPL = 6.19; %cm, effective length or Magnetic path lenght
+W_tfe = 13*2; %gr, Core Weight, (2*mass of core half)
+%W_tcu = ?; %gr, Copper Weight
+Ac = 0.832; %cm^2, Iron Area
+Ap = 0.59; %cm^4 for 0_43009EC Ferrite Core, Area Product
+Window_Area = Ap/Ac; %cm'2, Window Area
+G = 3.76; %cm, Winding Length
+MLT= 5.01; %cm, Mean length turn
+
+
+
+
+
 
 %Skin depth in centimeters
     %mu = 4*pi*(10^(-7)); %H/m permeability
@@ -24,17 +41,17 @@ Aw = (pi*(D^2))/4;
 %Step 1: Calculate the total period
 T = 1/f;
 %Step 2: Calculate the maximum transistor on time, t_on
-t_on = T*D_max;
+t_on_max = T*D_max;
 %Step 3-4-5: Calculate the total secondary load power
 P_out = 100; %W
-I_out = P_out/V_out;
-%Step 6: Calculate the maximum in put current
+I_out = P_out/(V_out);
+%Step 6: Calculate the maximum input current
 I_in_max = (P_out)/(V_in_min*n);
 %Step 7: Calculate the primary peak current
-I_p_peak = (2*P_out*T)/(n*V_in_min*t_on);
+I_p_peak = (2*P_out*T)/(n*V_in_min*t_on_max);
         %t_on_max ?
 %Step 8: Calculate the primary rms current
-I_p_rms = I_p_peak*sqrt(t_on/(3*T));
+I_p_rms = I_p_peak*sqrt(t_on_max/(3*T));
 %Step 9: Calculate the maximum input power
 P_in = P_out/n;
 %Step 10: Calculate the equivalent input resistance
@@ -47,18 +64,11 @@ Energy = (L_m*(I_p_peak^2))/2;
 Ke = 0.145*P_out*(B_max^2)*(10^(-4));
 %Step 14: Calculate the core geometry. See the design specification, 
 %window utilization factor, Ku ? 
-alfa = 1;
-Kg = (Energy^2)/(Ke*alfa); % What is alfa ?
+Kg = (Energy^2)/(Ke*alfa);
 %Step 15: Select an EFD core comparable in core geometry, Kg
 
 %Step 16: Calculate the current density, J, using a window utilization, 
-    %Ku = 0.29
     % Ku is the amount of copper that appears in the window area of the
-    % transformer or inductor
-    %Ap = Area product = WaAc factor
-    %Wa = Window area
-    %Ac = Iron area
-    WaAc = 0.59; %cm^4 for 0_43009EC Ferrite Core
 J = (2*Energy*(10^4))/(B_max*Ap*Ku);
 %Step 17: Calculate the primary wire area
 A_pw = I_p_rms/J;
@@ -70,61 +80,61 @@ S_np = ceil(A_pw/Aw_26);
 W_ap = Wa/2;
 N_p = ceil((Ku*W_ap)/(3*Aw_26));
 %Step 20: Calculate the required gap
-    Ac = WaAc/Wa;
-    MPL = 6.19; %cm, effective length or Magnetic path lenght
-lg = ((0.4*pi*(N_p^2)*Ac*(10^(-8)))/L_m)-((MPL)/(5900));
+lg = ((0.4*pi*(N_p^2)*Ac*(10^(-8)))/L_m)-((MPL)/(mu_m));
 %Step 21: Calculate the equivalent gap in mils
 mils = lg*393.7;
 %Step 22: Calculate the fringing flux factor
-    G = 3.76; %cm, Winding Length
 F = 1+((lg/(sqrt(Ac)))*(log((2*G)/lg)));
 %Step 23: Calculate the new number of turns, N_np, by inserting the
 %fringing flux, F
 N_np = ceil(sqrt((lg*L_m)/(0.4*pi*Ac*F*(10^(-8)))));
 %Step 24: Calculate the peak flux density, B_pk
-%------------------Tam anlayamad?m---------------------
-    mu_m = 1760; %Change when you choose the material type
 B_pk = (0.4*pi*N_np*F*I_p_peak*(10^(-4)))/(lg+(MPL/mu_m));
 %Step 25: Calculate the primary resistance per cm
-R_pcm = 1345/3;
+R_pcm = 1345/S_np;
 %Step 26: Calculate the primary winding resistance, R_p
-    MLT= 5.01; %cm, Mean length turn
 R_p = MLT*N_np*R_pcm*(10^(-6));
 %Step 27: Calculate the primary copper loss, P_p
 P_p = (I_p_rms^2)*R_p;
 %Step 28: Calculate the secondary turns, N_s1
-N_s1 = round((N_np*(V_out+V_d)*(1-D_max-D_w))/(V_in_min*D_max));
+N_s = round((N_np*(V_out-V_d)*(1-D_max*D_w))/(V_in_min*D_max));
 %Step 29: Calculate the secondary peak current, I_s1_peak
-I_s1_peak = (2*I_out)/(1-D_max-D_w);
+I_s_peak = (2*I_out)/(1-D_max-D_w);
 %Step 30: Calculate the secondary rms current
-I_s1_rms = (I_s1_peak)*sqrt((1-D_max*D_w)/3);
+I_s_rms = (I_s_peak)*sqrt((1-D_max*D_w)/3);
 %Step 31: Calculate the secondary wire area, A_sw1
-A_sw1 = I_s1_rms/J; %cm^2
+A_sw = I_s_rms/J; %cm^2
 %Step 32: Calculate the required number of secondary strands, S_ns1
-S_ns1 = ceil(A_sw1/Aw_26);
+S_ns = ceil(A_sw/Aw_26);
 %Step 33: Calculate the S1 secondary
-S1 = 1345/S_ns1;
+S1 = 1345/S_ns;
 %Step 34: Calculate winding resistance, R_s1
-R_s1 = MLT*N_s1*S1*(10^(-6));
+R_s = MLT*N_s*S1*(10^(-6));
 %Step 35: Calculate the secondary copper loss, P_s1
-P_s1 = (I_s1_rms^2)*R_s1;
-%Step 36: Calculate the secondary turns, N_s2
-N_s2 = ceil((N_np*(V_out+V_d)*(1-D_max*D_w))/(V_in_min*D_max));
-%Step 37: Calculate the secondary peak current, I_s2_peak
-I_s2_peak = (2*I_out)/(1-D_max*D_w);
-%Step 38: Calculate the secondary rms current, I_s2_rms
-I_s2_rms = I_s2_peak*sqrt((1-D_max*D_w)/3);
-%Step 39: Calculate the secondary wire area, A_sw2
-A_sw2 = I_s2_rms/J;
-%Step 40: Calculate the required number of secondary strands, S_ns2
-S_ns2 = ceil(A_sw2/Aw_26);
-
-
-
-
-
-
-
+P_s = (I_s_rms^2)*R_s;
+%Step 36-43: Skipped
+%Step 44: Calculate the window utilization, Ku
+Turns_p = N_p*S_np;
+Turns_s = N_s*S_ns;
+Nt = Turns_p+Turns_s; % Total number of turns with AWG #26
+Ku_final = (Nt*Aw)/Window_Area;
+%Step 45: Calculate the total copper loss, P_cu
+P_cu = P_p+P_s;
+%Step 46: Calculate the regulation, alfa, for this design
+alfa_final = (P_cu/P_out)*100;
+%Step 47: Calculate the ac flux density, B_ac
+B_ac = (0.4*pi*N_np*F*(I_p_peak/2)*(10^(-4)))/(lg+(MPL/mu_m));
+%Step 48: Calculate the watts per kilogram, WK
+WK = 4.855*(10^(-5))*(f^(1.63))*(B_ac^(2.62));
+%Step 49: Calculate the core loss, P_fe
+P_fe = WK*W_tfe*(10^(-3));
+%Step 50: Calculate the total loss, P_total_loss
+P_total_loss = P_fe+P_cu;
+%Step 51: Calculate the watt density
+    %At : Surface Area
+%Watt_density = P_total_loss/At;
+%Step 52: Calculate the temperature rise, Tr
+%Tr = 450*(Watt_density^(0.826));
 
 
 
