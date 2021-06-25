37363877d6a4526f180456a6a5436930c80f50e5
DESKTOP-HUBL1OU\Asus <busranur.kocak26@gmail.com>
2021-06-25 18:22:24 +0300

    Core loss calculations have been updated.

--- Simulations/Powder_Core_Calculations_v1.m
+++ Simulations/Powder_Core_Calculations_v1.m
@@ -1,6 +1,6 @@
 Pout = 100; %W, Ouput Power
 Vin_min = 220; %V, Minimum Input Voltage
-Dmax = 0.5; % (ton/(ton+toff)), Maximum Duty Cycle
+Dmax = 0.35; % (ton/(ton+toff)), Maximum Duty Cycle
 f = 100; %kHz, Switching Frequency
 Vout = 12; %V, Output Voltage
 n_eff = 0.9; %Efficiency
@@ -25,16 +25,15 @@ R = 0.1338568; %Ohms/m
 Imax = 0.361; %A, maximum current cable can carry
 
 % Number of 26 awg cable to be used
-Spri = ceil(Iave/Imax); 
+Spri = ceil(Iave/Imax);
 Ssec = ceil(Isec/Imax);
 
 % ?
 u0 = pi*4e-7;
 ucore = 90;
-le = 6.56; %cm, the path length in cm
+le = 6.56; %cm, the path length
 Ae = 0.601; %cm^2
 H = (Npri*Iave)/le;
-B = ucore*u0*H;
 Bdc = ucore*((0.4*pi*Npri*Iave)/(le))*1e-4;
 
 % E core window area calculation
@@ -46,15 +45,24 @@ Awindow = ((E-F)/2)*(2*D); %m^2
 Acopper = 0.129e-6; %m^2
 Kcu = (Acopper*((Spri*Npri)+(Ssec*Nsec)))/(83.3e-6);
 
-%Core Loss
-k = 0.000614;
-m = 1.460;
-n = 2;
-PL = k*((f*1e3)^m)*(B^n);
-Pfe = PL*le*Ae;
+Bac = (235*1e4)/(2*pi*f*(1e3)*Npri*Ae);
+Bpk = (Lpri*Ipk)/(Npri*Ae);
 
-%Copper Loss
+%Core Loss
+a = 146.81;
+b = 2.022;
+c = 1.33;
+PL = a*(Bpk^b)*(f^c);
+Pfe = PL*le*Ae*1e-3;
+
+%DC Copper Loss
 MLT = 2*pi*((F/2)+((E-F)/4)); %m
+Length1 = (Npri*MLT)*(Spri);
+Length2 = (Nsec*MLT)*(Ssec);
 Rpri = (Npri*MLT*R)/(Spri);
 Rsec = (Nsec*MLT*R)/(Ssec);
 Pcu = ((Iave^2)*Rpri)+((Isec^2)*Rsec);
+
+
+
+
